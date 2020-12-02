a46d4eefc91df16bb71518cfa430ab30c06ad643
Ridwan Abiola <ricklick262@gmail.com>
2020-12-02 21:12:31 +0100

    remove junk

--- src/redux/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-// import visibilityFilter from "./visibilityFilter";
-import products from "./products";
-import productsReducer from "./api";
-
-export default combineReducers({ products, productsReducer });
--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./reducers";
-import thunk from "redux-thunk";
-
-const middlewares = [thunk];
-
-export default createStore(rootReducer, applyMiddleware(...middlewares));
